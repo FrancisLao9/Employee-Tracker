e33613024e0190c4cbcb65638b1a04b2bcc44975
FrancisLao9 <lao.francis@gmail.com>
2023-01-08 10:16:49 -0500

    added sql files

---
+++ Main/db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT NOT NULL PRIMARY KEY,
+  dep_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL
+  salary DECIMAL NOT NULL,
+  deparment_id INT NOT NULL,
+  FOREIGN KEY (deparment_id)
+  REFERENCES department(dep_name)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL REFERENCES employee(id),
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+);
